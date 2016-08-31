2c485746aa3bbdad7d30519189f5109e8e907f1c
kvlinden <kvlinden@calvin.edu>
2016-08-31 17:33:33 -0400

    updated Ubuntu scripts...

--- src/startService.sh
+++ src/startService.sh
@@ -2,5 +2,5 @@
 
 # Run this script to compile/start the cs262 data service.
 
-javac -cp "../lib/*" edu/calvin/cs262/Player.java src/edu/calvin/cs262/MonopolyResource.java
+javac -cp "../lib/*" edu/calvin/cs262/Player.java edu/calvin/cs262/MonopolyResource.java
 java -cp ".:../lib/*" edu.calvin.cs262.MonopolyResource
